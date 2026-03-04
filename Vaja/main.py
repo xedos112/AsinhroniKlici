81677c81b9f318f598e8c8f23fbf20aa46cbfdd0
xedos112 <blazkavalar@gmail.com>
2026-03-04 07:56:47 +0100

    Add files via upload

---
+++ Vaja/main.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template, request
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/coinFlip")
+def coinFlip():
+    return render_template("coin.html")
+
+@app.route("/coinData")
+def coinData():
+    rnd=random.randint(0,1)
+    return ["TAILS", "HEADS"][rnd]
+
+if __name__ == "__main__":
+    app.run(debug=True)
